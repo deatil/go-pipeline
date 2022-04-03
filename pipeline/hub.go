2a6766b3a9018c80ac9bf6509947c59659ccd6ac
deatil <2217957370@qq.com>
2022-04-03 22:15:41 +0800

    更新

---
+++ pipeline/hub.go
@@ -0,0 +1,70 @@
+package pipeline
+
+import (
+    "sync"
+)
+
+var instanceHub *Hub
+var onceHub sync.Once
+
+// 构造函数
+func NewHub() *Hub {
+    return &Hub{
+        Pipelines: make(HubPipelinesMap),
+    }
+}
+
+// 单例
+func InstanceHub() *Hub {
+    onceHub.Do(func() {
+        instanceHub = NewHub()
+    })
+
+    return instanceHub
+}
+
+type (
+    // 回调函数
+    HubCallbackFunc = func(*Pipeline, interface{}) interface{}
+
+    // 数据列表
+    HubPipelinesMap = map[string]HubCallbackFunc
+)
+
+/**
+ * Hub
+ *
+ * @create 2022-2-8
+ * @author deatil
+ */
+type Hub struct {
+    // 设置的数据
+    Pipelines HubPipelinesMap
+}
+
+// 默认
+func (this *Hub) Defaults(callback HubCallbackFunc) *Hub {
+    return this.Pipeline("default", callback)
+}
+
+// 设置
+func (this *Hub) Pipeline(name string, callback HubCallbackFunc) *Hub {
+    this.Pipelines[name] = callback
+
+    return this
+}
+
+// 执行
+func (this *Hub) Pipe(object interface{}, pipeline ...string) interface{} {
+    name := "default"
+
+    if len(pipeline) > 0 {
+        name = pipeline[0]
+    }
+
+    if pipelineCallback, ok := this.Pipelines[name]; ok {
+        return pipelineCallback(NewPipeline(), object)
+    }
+
+    return nil
+}
